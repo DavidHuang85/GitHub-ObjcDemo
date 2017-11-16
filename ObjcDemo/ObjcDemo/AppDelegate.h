d213bacb609e58762088d40c925cd191954664d8
黄金龙 <huangjl1275@13322.com>
2017-11-16 16:29:43 +0800

    添加podfile文件

---
+++ ObjcDemo/ObjcDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcDemo
+//
+//  Created by davidhuang on 2017/11/16.
+//  Copyright © 2017年 davidhuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcDemo/ObjcDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ObjcDemo
+//
+//  Created by davidhuang on 2017/11/16.
+//  Copyright © 2017年 davidhuang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
