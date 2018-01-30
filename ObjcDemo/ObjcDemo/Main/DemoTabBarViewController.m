d9f354944becd896a6b1b3ce9d7189a1ad6bcbc0
黄金龙 <huangjl1275@13322.com>
2018-01-30 15:54:13 +0800

    tabBar的背景颜色

--- ObjcDemo/ObjcDemo/Main/DemoTabBarViewController.m
+++ ObjcDemo/ObjcDemo/Main/DemoTabBarViewController.m
@@ -21,6 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.navigationItem.title = nil;
+    self.tabBar.backgroundColor = [UIColor blackColor];
 
     DemoFirstViewController *f = [[DemoFirstViewController alloc] init];
     f.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"First" image:nil selectedImage:nil];
@@ -37,7 +38,32 @@
     t.view.backgroundColor = [UIColor blueColor];
     UINavigationController *nav3 = [[UINavigationController alloc] initWithRootViewController:t];
     self.viewControllers = @[nav1,nav2,nav3];
+
+
+    [self.tabBar setBackgroundImage:[self imageWithColor:[UIColor whiteColor]]];
 }
+
+
+- (UIImage *)imageWithColor:(UIColor *)color {
+    // 描述矩形
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+
+    // 开启位图上下文
+    UIGraphicsBeginImageContext(rect.size);
+    // 获取位图上下文
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    // 使用color演示填充上下文
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    // 渲染上下文
+    CGContextFillRect(context, rect);
+    // 从上下文中获取图片
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    // 结束上下文
+    UIGraphicsEndImageContext();
+
+    return theImage;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
